725df6c020995daf9ac4cf640b2adcbf84c52fb1
Matthew Ao <37258460+aoruize@users.noreply.github.com>
2019-08-08 09:26:58 -0400

    Add files via upload

---
+++ Program.java
@@ -0,0 +1,6 @@
+class Program{
+  public static void main(String[] args){
+    SportsCar bugatti = new SportsCar("Volkswagen", "Bugatti Veyron", 100, 400, 1700000, 17);
+    System.out.println(bugatti.range());
+  }
+}
