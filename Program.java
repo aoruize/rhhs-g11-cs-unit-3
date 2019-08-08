4aaddfe4a2e9b2de15ff2e68f92472da77e3fe42
Matthew Ao <37258460+aoruize@users.noreply.github.com>
2019-08-08 09:28:06 -0400

    Delete Program.java

--- Program.java
+++
@@ -1,6 +0,0 @@
-class Program{
-  public static void main(String[] args){
-    SportsCar bugatti = new SportsCar("Volkswagen", "Bugatti Veyron", 100, 400, 1700000, 17);
-    System.out.println(bugatti.range());
-  }
-}
